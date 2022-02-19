88a1258b54b43b9f961feb92c1b07f6bd51845dc
Mario Kaufmann <mario.kaufmann@bluemail.ch>
2022-02-19 10:21:27 +0100

    Fix output root element

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io::Write;
 use std::path::Path;
 
 use anyhow::{anyhow, Context};
@@ -8,6 +9,24 @@ mod cli;
 mod cobertura;
 mod jacoco;
 mod map;
+
+fn convert(input_file: &str, output_file: &str, source_root: Vec<String>) -> anyhow::Result<()> {
+    let input_text = std::fs::read_to_string(input_file).context("Could not read input file.")?;
+    let jacoco_report: JacocoReport =
+        quick_xml::de::from_str(&input_text).context("Could not parse Jacoco report.")?;
+    let cobertura_report = map::map(jacoco_report, source_root)
+        .context("Could not map Jacoco report to Cobertura report.")?;
+
+    let output = quick_xml::se::to_string(&cobertura_report)
+        .context("Could not serialize Cobertura report.")?;
+    let mut file = std::fs::File::create(output_file).context("Could not create output file.")?;
+    file.write_all("<?xml version=\"1.0\" ?>".as_bytes())
+        .context("Could not write header into output file.")?;
+    file.write_all(output.as_bytes())
+        .context("Could not write report into output file.")?;
+
+    Ok(())
+}
 
 fn main() -> anyhow::Result<()> {
     let args = cli::parse_args();
@@ -21,17 +40,7 @@ fn main() -> anyhow::Result<()> {
         };
     }
 
-    let input_text =
-        std::fs::read_to_string(&args.input_file).context("Could not read input file.")?;
-    let jacoco_report: JacocoReport =
-        quick_xml::de::from_str(&input_text).context("Could not parse Jacoco report.")?;
-    let cobertura_report = map::map(jacoco_report, args.source_root)
-        .context("Could not map Jacoco report to Cobertura report.")?;
-
-    let output = quick_xml::se::to_string(&cobertura_report)
-        .context("Could not serialize Cobertura report.")?;
-    std::fs::write(&args.output_file, output)
-        .context("Could not write Cobertura report to output file.")?;
+    convert(&args.input_file, &args.output_file, args.source_root)?;
 
     Ok(())
 }
@@ -39,8 +48,6 @@ fn main() -> anyhow::Result<()> {
 #[cfg(test)]
 mod tests {
     use std::path::PathBuf;
-
-    use pretty_assertions::assert_eq;
 
     use super::*;
 
@@ -50,16 +57,23 @@ mod tests {
         directory.push("fixtures");
         let mut input_file_path = directory.clone();
         input_file_path.push("jacoco_input.xml");
-        let input_text = std::fs::read_to_string(input_file_path).unwrap();
-        let jacoco_report: JacocoReport = quick_xml::de::from_str(&input_text).unwrap();
-        let cobertura_report =
-            map::map(jacoco_report, vec!["project/src/main/java".to_owned()]).unwrap();
-        let mut output_file_path = directory;
-        output_file_path.push("expected_cobertura_output.xml");
-        let output_text = quick_xml::se::to_string(&cobertura_report).unwrap();
+        let input_file_path = input_file_path.as_os_str().to_str().unwrap();
 
-        let expected_text = std::fs::read_to_string(output_file_path).unwrap();
+        convert(
+            input_file_path,
+            "output.xml",
+            vec!["project/src/main/java".to_owned()],
+        )
+        .unwrap();
 
-        assert_eq!(output_text, expected_text);
+        let mut expected_output_file_path = directory;
+        expected_output_file_path.push("expected_cobertura_output.xml");
+        let expected_output_file_path = expected_output_file_path.as_os_str().to_str().unwrap();
+        let expected_text = std::fs::read_to_string(expected_output_file_path).unwrap();
+        let actual_text = std::fs::read_to_string("output.xml").unwrap();
+
+        std::fs::remove_file("output.xml").unwrap();
+
+        assert_eq!(actual_text, expected_text);
     }
 }
